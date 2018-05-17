ff55db53cab147c2ea4c47d566243696c49085b8
Shanny <648751764@qq.com>
2018-05-17 13:43:45 +0800

    button

---
+++ 2_button/New Ball/Assets/Script/button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public GameObject camera1;
+    public RenderTexture minimap;
+
+    public void Click()
+    {
+        if (mainCamera.activeInHierarchy)
+        {
+            mainCamera.GetComponent<Camera>().targetTexture = minimap;
+            camera1.GetComponent<Camera>().targetTexture = null;
+        } else
+        {
+            mainCamera.GetComponent<Camera>().targetTexture = null;
+            camera1.GetComponent<Camera>().targetTexture = minimap;
+        }
+
+        Debug.Log("Button Clicked. TestClick.");
+    }
+}
