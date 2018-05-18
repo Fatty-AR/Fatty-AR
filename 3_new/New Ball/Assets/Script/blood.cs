e4c6fe767450686041ee5c2aea4316b01cf6a8c7
Cicicigar <caosj3@mail2.sysu.edu.cn>
2018-05-18 17:02:15 +0800

    修改墙及环形进度条

---
+++ 3_new/New Ball/Assets/Script/blood.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class blood : MonoBehaviour
+{
+	public Scrollbar m_bar;
+	private int i = 0;
+    // Use this for initialization
+    void Start()
+    {
+		m_bar.size = 100;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+		i++;
+		if (i % 20 == 0) {
+			m_bar.size -= 0.01f;
+		}
+    }
+}
