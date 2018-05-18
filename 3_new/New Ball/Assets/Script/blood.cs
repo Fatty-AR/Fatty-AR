3df50163dc0f4a26b8e9aa2c6520125895f64751
Czjcssy <1106713319@qq.com>
2018-05-18 20:15:29 +0800

    吃-满意度条增

--- 3_new/New Ball/Assets/Script/blood.cs
+++ 3_new/New Ball/Assets/Script/blood.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections;
-using UnityEditor;
 using UnityEngine.UI;
 
 public class blood : MonoBehaviour
--- 3_new/New Ball/Assets/eatbuttonclick.cs
+++ 3_new/New Ball/Assets/eatbuttonclick.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class eatbuttonclick : MonoBehaviour {
 
 	public GameObject circle;
+	public Scrollbar satisfiedBar, healthBar;
+	private ColorBlock cb;
 
     void Start () {
         Button btn = this.GetComponent<Button> ();
@@ -14,6 +16,13 @@ public class eatbuttonclick : MonoBehaviour {
 
     private void OnClick(){
     	circle.SetActive(true);
+    	satisfiedBar.size += 0.2f;
+    	healthBar.size += 0.05f;
+    	if (satisfiedBar.size >= 0.75f) {
+    		cb = satisfiedBar.colors;
+    		cb.normalColor = Color.red;
+    		satisfiedBar.colors = cb;
+    	}
         Debug.Log ("Button Clicked. ClickHandler.");
     }
 
