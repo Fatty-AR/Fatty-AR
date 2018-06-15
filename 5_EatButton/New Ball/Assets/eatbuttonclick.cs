8ea93039cf19868a456762330c5ae469f1392353
Czjcssy <1106713319@qq.com>
2018-06-15 23:53:22 +0800

    更改吃按钮
    1.eatButton只有在靠近食物的时候可以按
    2.吃食物的时候不能动，按钮也不让按
    3.上一次吃过的食物不能连续吃两次。

---
+++ 5_EatButton/New Ball/Assets/eatbuttonclick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eatbuttonclick : MonoBehaviour {
+	public GameObject circle;
+    private Button thisButton;
+    private GameObject FoodRoot, NearestFood;
+    private GameObject lastAteFood = null;//记录上一次吃过的食物 不可原地连吃两次
+
+    void Start () {
+        thisButton = this.GetComponent<Button> ();
+        thisButton.onClick.AddListener (OnClick);
+        thisButton.interactable = false;
+        FoodRoot = GameObject.Find("ImageTarget/SilverTraySq");
+    }
+
+    private void OnClick(){
+        if (lastAteFood != NearestFood && NearestFood.activeInHierarchy)
+        {
+            circle.SetActive(true);
+            lastAteFood = NearestFood;
+            Debug.Log("Eat "+lastAteFood.name);
+        }
+    	
+    }
+
+    private void Update()
+    {
+        //盘中食物的根节点
+        GameObject root = GameObject.Find("ImageTarget");
+        Vector3 playerPos; //用于存储两者坐标
+
+        playerPos = root.transform.Find("MobileMaleFreeSimpleMovement1").gameObject.transform.position;
+        NearestFood = GetNearestFood(playerPos);
+        float leastDis = Vector3.Distance(playerPos, NearestFood.transform.position);
+        if (leastDis < 0.9 && lastAteFood != NearestFood &&NearestFood.activeInHierarchy)
+        {
+            thisButton.interactable = true;
+        } else
+        {
+            thisButton.interactable = false;
+        }
+    }
+
+    private GameObject GetNearestFood(Vector3 playerPosition)
+    {
+        Vector3 foodPosition;
+        float leastDistance = 10000;
+        int i,least = 0;
+        int foodCount = FoodRoot.transform.childCount;
+        for (i = 0; i < foodCount; i++)
+        {
+            foodPosition = FoodRoot.transform.GetChild(i).gameObject.transform.position;
+            float dis = Vector3.Distance(playerPosition, foodPosition);
+            if (dis < leastDistance)
+            {
+                leastDistance = dis;
+                least = i; 
+            }
+        }
+        return FoodRoot.transform.GetChild(least).gameObject;
+    }
+
+}
---
+++ 5_EatButton/New Ball/Assets/ring.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ring : MonoBehaviour {
+
+	[SerializeField]
+	float speed;
+	
+	[SerializeField]
+	Transform process;
+	
+	[SerializeField]
+	Transform indicator;
+	
+	public int targetProcess{ get; set;}
+	private float currentAmout = 0;
+    public ScrollCircle controller;//虚拟摇杆
+    public Button switchButton;//切换视角的按钮
+    public Scrollbar satisfiedBar, healthBar;
+    public GameObject circle;
+    private ColorBlock cb;
+
+    // Use this for initialization
+    void Start () {
+		currentAmout = 0;
+		targetProcess = 100;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (currentAmout < targetProcess) {
+            controller.enabled = false;
+            switchButton.interactable = false;
+            //Debug.Log("currentAmount:" + currentAmout.ToString());
+            currentAmout += speed;
+			if(currentAmout > targetProcess)
+				currentAmout = targetProcess;
+			indicator.GetComponent<Text>().text = ((int)currentAmout).ToString() + "%";
+			process.GetComponent<Image>().fillAmount = currentAmout/100.0f;
+		} else {
+            satisfiedBar.size += 0.08f;
+            healthBar.size += 0.05f;
+            if (satisfiedBar.size >= 0.75f)
+            {
+                cb = satisfiedBar.colors;
+                cb.normalColor = Color.red;
+                satisfiedBar.colors = cb;
+            }
+            currentAmout = 0;
+            controller.enabled = true;
+            switchButton.interactable = true;
+            circle.SetActive(false);
+		}
+		
+	}
+
+	
+	public void SetTargetProcess(int target)
+	{
+		if(target >= 0 && target <= 100)
+			targetProcess = target;
+	}
+}
