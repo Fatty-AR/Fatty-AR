a4709e5a64b3fee7bff7eff1d91ca66c90da07b1
Czjcssy <1106713319@qq.com>
2018-06-19 17:35:24 +0800

    7
    修改了吃东西吃不到的问题，部分buff时间延长。

---
+++ 7_FixEatBug/New Ball/Assets/Script/ScrollCircle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollCircle : ScrollRect {
+    float radius = 0;
+    public Vector2 distance;
+	// Use this for initialization
+	void Start () {
+        distance = new Vector2();
+        radius = (transform as RectTransform).sizeDelta.x * 0.45f;
+	}
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        Vector2 pos = content.anchoredPosition;
+        if (pos.magnitude > radius)
+        {
+            pos = pos.normalized * radius;
+            SetContentAnchoredPosition(pos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        distance = content.localPosition / radius;
+        //Debug.Log(distance);
+	}
+}
---
+++ 7_FixEatBug/New Ball/Assets/Script/blood.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blood : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera camera1;
+    public Scrollbar m_bar;
+    public GameObject player;
+	private int i = 0;
+    public UnityEngine.Canvas m_canvas;//画布  
+
+    // Use this for initialization
+    void Start()
+    {
+		m_bar.size = 1;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (mainCamera.GetComponent<Camera>().targetTexture == null) //全局视角
+        {
+            //目标世界坐标转画布坐标  
+            Vector3 worldToScreenPoint = mainCamera.WorldToScreenPoint(player.transform.position);
+            //在画布上对应的点  
+            worldToScreenPoint = new Vector3(worldToScreenPoint.x, worldToScreenPoint.y, m_canvas.planeDistance);
+            Vector3 screenToWorldPoint = mainCamera.ScreenToWorldPoint(worldToScreenPoint);
+            //得到最终画布坐标系中的投影点  
+            Vector3 projPoint = m_canvas.transform.worldToLocalMatrix.MultiplyPoint(screenToWorldPoint);
+
+            this.transform.localPosition = projPoint;
+        } else {
+
+        }*/
+        i++;
+        if (i % 50 == 0)
+        {
+            m_bar.size -= 0.01f;
+        }
+    }
+
+    public float getBlood()
+    {
+        return m_bar.size;
+    }
+
+    public void addBlood(float value)
+    {
+        m_bar.size += value;
+    }
+
+    public void minusBlood(float value)
+    {
+        m_bar.size -= value;
+    }
+
+}
