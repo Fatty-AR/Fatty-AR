ce8696d76ffc44414d4012e814d3ea2db6d79ee1
Shanny <648751764@qq.com>
2018-06-21 18:29:21 +0800

    fork

---
+++ 11_Fork/New Ball/Assets/Script/button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public GameObject camera1;
+    public RenderTexture minimap;
+	public bool firstPerson;
+	public GameObject scroll;
+
+	public void Start() {
+		firstPerson = false;
+        scroll.SetActive(false);
+    }
+
+    public void Click()
+    {
+        if (mainCamera.GetComponent<Camera>().targetTexture == null)
+        {
+            scroll.SetActive(true);
+            firstPerson = true;
+            mainCamera.GetComponent<Camera>().targetTexture = minimap;
+            camera1.GetComponent<Camera>().targetTexture = null;
+        } else
+        {
+            scroll.SetActive(false);
+            firstPerson = false;
+            mainCamera.GetComponent<Camera>().targetTexture = null;
+            camera1.GetComponent<Camera>().targetTexture = minimap;
+        }
+
+        Debug.Log("Button Clicked. TestClick.");
+    }
+}
