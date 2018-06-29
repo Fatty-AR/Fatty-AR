4c1a7ec5435a9b189a14b9e3dc4f554b04920459
Czjcssy <1106713319@qq.com>
2018-06-29 15:55:54 +0800

    16

---
+++ 16_NeedWinScene/New Ball/Assets/Script/attack_mark.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class attack_mark : MonoBehaviour
+{
+    private int transparent = 1;
+    public Image attackMark;
+    private Color color;
+    private int i = 0;
+    private bool EableMarkFlash = false;
+    private bool NoticeBuff = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        color = attackMark.color;
+        color.a = 0;
+        attackMark.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EableMarkFlash && NoticeBuff)
+        {
+            i = (i + 1) % 1000;
+            if (i % 3 == 0)
+            {
+                transparent ^= 1;
+                if (transparent == 0)
+                {
+                    color.a = 0;
+                }
+                else
+                {
+                    color.a = 1;
+                }
+                attackMark.color = color;
+            }
+        }
+    }
+
+    public void MarkFlash() //开始 叉子警告闪烁
+    {
+        EableMarkFlash = true;
+    }
+
+    public void DisableMarkFlash()// 关闭 叉子警告闪烁
+    {
+        EableMarkFlash = false;
+        color.a = 0;
+        attackMark.color = color;
+    }
+
+    public bool GetNoticeBuff() //return true 表示已获得“叉子警告”
+    {
+        return NoticeBuff;
+    }
+
+    public void SetNoticeBuff(bool buff)
+    {
+        NoticeBuff = buff;
+    }
+}
---
+++ 16_NeedWinScene/New Ball/Assets/Script/blood.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class blood : MonoBehaviour
+{
+    //public Camera mainCamera;
+    //public Camera camera1;
+    public Scrollbar m_bar;
+    public GameObject player;
+	private int i = 0;
+    public UnityEngine.Canvas m_canvas;//»­²¼  
+    public Vuforia.DefaultTrackableEventHandler targetScript;
+    private ColorBlock cb;
+
+    // Use this for initialization
+    void Start()
+    {
+		m_bar.size = 1;
+        cb = m_bar.colors;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetScript.start)
+        {
+            i++;
+            if (i % 50 == 0)
+            {
+                m_bar.size -= 0.01f;
+            }
+            if (m_bar.size <= 0.3)
+            {
+                cb.normalColor = Color.red;
+                m_bar.colors = cb;
+            }
+            if (m_bar.size == 0)
+            {
+                SceneManager.LoadScene("endgame");
+            }
+        }
+        
+    }
+
+    public float getBlood()
+    {
+        return m_bar.size;
+    }
+
+    public void addBlood(float value)
+    {
+        m_bar.size += value;
+    }
+
+    public void minusBlood(float value)
+    {
+        m_bar.size -= value;
+    }
+
+}
